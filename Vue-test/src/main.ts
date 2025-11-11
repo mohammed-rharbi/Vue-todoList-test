b27fa1ce90bae8ffe7d642efbc9266c12337503f
mohammed rharbi <rharbi383@gmail.com>
2025-11-11 15:36:05 +0100

    refactor: update app entry point in main.ts

--- Vue-test/src/main.ts
+++ Vue-test/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+// @ts-ignore: TS cannot find declaration file for .vue SFCs
 import App from './App.vue'
+// @ts-ignore: TS cannot find declaration file for module './router'
+import router from './router'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
